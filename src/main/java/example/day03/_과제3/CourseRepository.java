073a36e535255dfa99113d9c35e835b29800f9d2
devcube2 <jejeje8@knou.ac.kr>
2025-04-10 12:54:29 +0900

    2025-04-10 수업 소스 커밋

--- src/main/java/example/day03/_과제3/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package example.day03._과제3;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<CourseEntity, Integer> {
-}
